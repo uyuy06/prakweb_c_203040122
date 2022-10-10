11e8c437c5aeeadfc4d84f8597f40f2f4ba7f930
fdlashari <acilliitle166@gmail.com>
2022-10-10 10:53:50 +0700

    pendahuluan-view

---
+++ mvc/app/views/about/index.php
@@ -0,0 +1,3 @@
+
+    <h1>About Me</h1>
+    <p>Halo, Nama saya <?= $data['nama'];?>, Umur saya <?= $data ['umur']; ?> Tahun, Saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
---
+++ mvc/app/views/home/index.php
@@ -0,0 +1,2 @@
+
+    <h1>Selamat datang di Website Saya</h1>
